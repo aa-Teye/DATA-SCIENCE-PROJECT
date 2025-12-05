bfd2c53d1752aeba510f35c2865ebc6dda727410
aa-Teye <alexteyeametepey@gmail.com>
2025-12-05 01:29:41 +0000

    PERSONALIZE MESSAGE GENERATOR PROJECT2

---
+++ Exercise3.py
@@ -0,0 +1,16 @@
+# Project 2 
+
+#WELCOME MESSAGE GENERATOR
+#STEP1 Ask for user details
+name = input("Enter your name:  ")
+hobby = input("Enter your hobby:  ")
+
+#STEP 2: Generate a personalize welcome message 
+print("\n --- Welcome Message ---") 
+print(f"Hello,{name}!")
+print(f"Welcome to the world of python programming.")
+print(f"It's great to know that you love {hobby}.")
+print(f"Get ready to build something amazing today.")
+
+
+
