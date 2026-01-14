93d8e635f7972fe23e38f8ac9e7e49fd98fb6b05
aa-Teye <alexteyeametepey@gmail.com>
2026-01-14 16:14:27 +0000

    Second Commit

--- ShoppingListPlayGround.py
+++ ShoppingListPlayGround.py
@@ -20,3 +20,17 @@ print("\n --- Bulleting the list with numbers: ---")
 for index, item in enumerate(shopping_list):
     print(f"{index +1} {item} is in the shopping list")
 
+
+
+ #Creating A Onling Shopping Cart Logic Application 
+#Step 1 Creating the empty cart to add items to purchase
+cart = []
+
+#Step2 Create the Continuous Loop Menu to Display Options to User 
+def cart_menu():
+    print("\n --- Shopping Cart Menu  ---")
+    print(1. View Cart)
+    print(2. Add Item)
+    print(3.Remove Item)
+    print(4. Empty Cart)
+    print(5. Exit)
