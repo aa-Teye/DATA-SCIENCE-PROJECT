ca10abd5e24f5daee3023ef085bd74efc9959fde
aa-Teye <alexteyeametepey@gmail.com>
2026-02-04 23:04:22 +0000

    Second commit

--- #WORKING WITH TUPLES.py
+++ #WORKING WITH TUPLES.py
@@ -4,3 +4,11 @@ my_List = [1, 2, 3, 7]
 print(my_Tuples)
 print(type(my_Tuples))
 print(type(my_List))
+print(my_Tuples[0])
+print(my_Tuples[-1])
+
+components = (2,3,5)
+a , b, c = components
+print(a)
+print(b)
+print(c)
